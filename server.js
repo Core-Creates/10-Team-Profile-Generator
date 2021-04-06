7568c7436a2ff3a8b6628aabb62108c306d2ac3f
Corrina Alcoser <corrinaalcoser@gmail.com>
2021-04-05 23:57:14 -0500

    edited html append items

--- server.js
+++ server.js
@@ -30,9 +30,17 @@ fs.writeFile(fileName,
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Teammates</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+
 </head>
 
-<body> \n`,
+<body>
+<nav className="navbar navbar-light bg-light"> <div class="container-fluid">
+<a class="navbar-brand" href="#">Teammate website</a>
+</div>
+<Nav> \n`,
     function (err) {
         if (err) throw err;
         console.log('Created!');
@@ -48,8 +56,8 @@ userInput.question("How Many people are on your team? ", function (teamNumber) {
 
     // userInput.next(teamNumber);
     for (let i = 0; i < teamNumber; i++) {
-        fs.appendFile(fileName, "<div class='card' style='width: 18rem;'>", function () { });
-        fs.appendFile(fileName, '<div class="card-body">', function () { });
+        fs.appendFile(fileName, "<div class='card' style='width: 18rem;'>\n", function () { });
+        fs.appendFile(fileName, '<div class="card-body">\n', function () { });
 
         userInput.question("What is your name ? ", function (name) {
             // userInput.next(name);
@@ -75,6 +83,8 @@ userInput.question("How Many people are on your team? ", function (teamNumber) {
                             your office number is ${officeNum},
                             your github is ${github},
                             your school is ${school}`)
+
+                            fs.appendFile(fileName, "</div>\n </div>\n </body>\n </html>\n", function () { });
 
                             userInput.close();
                         });
