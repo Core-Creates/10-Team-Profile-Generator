9fb5f5e9e319d787fa506237cc8a683d1b8b2165
Corrina Alcoser <corrinaalcoser@gmail.com>
2021-04-05 22:14:06 -0500

    added user terminal prompt

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const fs = require('fs');
 const path = require('path');
 const http = require('http');
 
+const readline = require("readline");
+
+
+
 http.createServer(function (req, res) {
     fs.readFile('demofile1.html', function (err, data) {
         if (err) {
@@ -15,3 +19,32 @@ http.createServer(function (req, res) {
         }
     });
 }).listen(8080);
+
+fs.writeFile('teamMates.html', function (err) {
+    if (err) throw err;
+    console.log('Created!');
+    
+});
+
+
+const userInput = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+userInput.question("How Many people are on your team? ", function (teamNumber) {
+    for (let i = 1; i <= teamNumber; i++) {
+        userInput.question("What is your name ? ", function (name) {
+            userInput.question("What is your email ? ", function (email) {
+                userInput.question("What is your Office Number? ", function (officeNum) {
+                    userInput.question("What is your github?", function (github) {
+                        userInput.question("What school do you attend? ", function (school) {
+                            userInput.close();
+                        });
+                    });
+                });
+            });
+        });
+    }
+});
+
